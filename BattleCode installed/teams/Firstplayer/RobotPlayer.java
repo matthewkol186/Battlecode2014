f9e9724291a84617f623999412d8de3c3d0b0df6
Matthew Sun <matthewkol186@gmail.com>
2014-01-22 19:29:46 -0500

    The full Battlecode folder

---
+++ BattleCode installed/teams/Firstplayer/RobotPlayer.java
@@ -0,0 +1,15 @@
+package Firstplayer;
+
+import battlecode.common.*;
+
+public class RobotPlayer
+{
+	public static void run(RobotController rc)
+	{
+		while(true)
+		{
+			//some computation
+			rc.yield();
+		}
+	}
+}
